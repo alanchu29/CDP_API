17f9f31bc8e5a70d3f30762122c1c6d112335095
Dixon Chu <Dixon_chu@wiwynn.com>
2026-04-22 16:17:59 -0600

    Add 'WYMUS' to site options in query_api.py for expanded selection

--- query_api.py
+++ query_api.py
@@ -319,7 +319,7 @@ st.markdown("<p style='text-align: center; color: #8b949e;'>Enter serial numbers
 with st.sidebar:
     st.subheader("⚙️ Settings")
     
-    site_options = ['WCZ', 'WYMY', 'WYMX', 'WYTN']
+    site_options = ['WCZ', 'WYMY', 'WYMX', 'WYTN', 'WYMUS']
     site = st.selectbox(
         "SITE",
         options=site_options,
